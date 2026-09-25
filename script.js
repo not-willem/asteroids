664562f35cec5ee0f225238326994fb0bd19c173
not-willem <vanhuwi26034@whs.school.nz>
2026-09-26 11:32:31 +1200

    movemntn

--- script.js
+++ script.js
@@ -4,40 +4,49 @@ const img = document.getElementById("playerguy");
 const playerspeed = 20;
 ctx.scale(0.1, 0.1);
 const keysPressed = {};
-if (img.complete) {
-  ctx.drawImage(img, 0, 0);
-} else {
-  img.onload = function() {
-    ctx.drawImage(img, 0, 0);
-    
-  };
-}
-document.addEventListener('keydown', (event) => {
-  keysPressed[event.key] = true;
-    if (keysPressed['a']) { angle -= 0.05; }
-    if (keysPressed['d']) { angle += 0.05; }
-    if (keysPressed['w']) { speed = playerspeed; }
-    if (keysPressed['s']) { speed = -playerspeed; }
-});
-document.addEventListener('keyup', (event) => {
-  keysPressed[event.key] = false;
-  if (keysPressed != true) {
-    speedx=0
-    speedy=0
-  }
 
+if (img.complete) {
+    ctx.drawImage(img, 0, 0);
+} else {
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+    };
+}
+
+document.addEventListener('keydown', (event) => {
+    keysPressed[event.key] = true;
+    if (keysPressed['d']) {
+        speedx = playerspeed;
+    }
+    if (keysPressed['a']) {
+        speedx = 0 - playerspeed;
+    }
+    if (keysPressed['s']) {
+        speedy = playerspeed;
+    }
+    if (keysPressed['w']) {
+        speedy = 0 - playerspeed;
+    }
 });
+
+document.addEventListener('keyup', (event) => {
+    keysPressed[event.key] = false;
+    if (keysPressed != true) {
+        speedx = 0
+        speedy = 0
+    }
+});
+
 window.addEventListener('blur', () => {
-  for (let key in keysPressed) {
-    keysPressed[key] = false;
-  }
+    for (let key in keysPressed) {
+        keysPressed[key] = false;
+    }
 });
 
 let playerx = 0
 let playery = 0
-let angle = 0;
-let speed = 0;
-
+let speedx = 0
+let speedy = 0
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width * 10, canvas.height * 10);
@@ -46,4 +55,5 @@ function animate() {
     playery += speedy;
     requestAnimationFrame(animate);
 }
+
 animate()
